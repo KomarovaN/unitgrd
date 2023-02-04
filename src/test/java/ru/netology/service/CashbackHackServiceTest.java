2d1aa5aa9f838c50fd21091eea4d8a7b5125db07
Nadezhda Komarova <grukhinan@gmail.com>
2023-02-04 21:47:29 +0300

    Changed dependencies,uses the JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,28 +1,28 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldRemainsIfAmountLessBoundary() {
-       Assertions.assertEquals(100, service.remain(900));
+       assertEquals(100, service.remain(900));
     }
 
     @Test
     public void shouldNullIfAmountIsBoundary() {
-        Assertions.assertEquals(0, service.remain(1000));
+        assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void shouldNullIfAmountWithoutRemainderBoundary() {
-        Assertions.assertEquals(0, service.remain(2000));
+        assertEquals(0, service.remain(2000));
     }
 
     @Test
     public void shouldNullIfAmountMoreBoundary() {
-       Assertions.assertEquals(999, service.remain(1001));
+      assertEquals(999, service.remain(1001));
     }
 }
