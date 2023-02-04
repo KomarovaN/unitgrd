788f163daf03e50e4b51a609ab3de40109840803
Nadezhda Komarova <grukhinan@gmail.com>
2023-02-04 20:43:25 +0300

    Changed and added tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,22 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
+    private CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldRemainsIfAmountLessBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        Assertions.assertEquals(100, service.remain(900));
+       Assertions.assertEquals(100, service.remain(900));
     }
 
     @Test
-    public void shouldNullIfAmountBoundary() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldNullIfAmountIsBoundary() {
         Assertions.assertEquals(0, service.remain(1000));
     }
 
     @Test
+    public void shouldNullIfAmountWithoutRemainderBoundary() {
+        Assertions.assertEquals(0, service.remain(2000));
+    }
+
+    @Test
     public void shouldNullIfAmountMoreBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        Assertions.assertEquals(999, service.remain(1001));
+       Assertions.assertEquals(999, service.remain(1001));
     }
 }
